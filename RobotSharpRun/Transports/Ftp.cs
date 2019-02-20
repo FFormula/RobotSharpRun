3d9a398e86b7b7287955c5d93b155ae42316b5da
Jevgenij Volosatov <fformula@gmail.com>
2019-02-20 19:48:53 +0200

    Transport files

---
+++ RobotSharpRun/Transports/Ftp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace RobotSharpRun.Transports
+{
+    class Ftp : Transport
+    {
+        FtpDriver driver;
+
+        public Ftp(FtpDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public string GetNextRunkey()
+        {
+            foreach (string folder in driver.Dir("wait/"))
+                if (folder[0] != '.')
+                    return folder;
+            return null;
+        }
+
+        public void GetWorkFiles(string runkey, string toFolder)
+        {
+            Directory.CreateDirectory(toFolder + runkey);
+            driver.DownloadFolder("wait/" + runkey + "/", toFolder + runkey + "\\");
+            driver.Rename("wait/" + runkey, "../work/" + runkey);
+        }
+
+        public void PutDoneFiles(string runkey, string fromFolder)
+        {
+            foreach (string fileOut in Directory.GetFiles(fromFolder + runkey, "*.out"))
+                driver.Put(fileOut, "work/" + runkey + "/" + Path.GetFileName(fileOut));
+            driver.Rename("work/" + runkey, "../done/" + runkey);
+        }
+    }
+}
---
+++ RobotSharpRun/Transports/Transport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSharpRun.Transports
+{
+    interface Transport
+    {
+        string GetNextRunkey();
+        void GetWorkFiles(string runkey, string toFolder);
+        void PutDoneFiles(string runkey, string fromFolder);
+    }
+}
