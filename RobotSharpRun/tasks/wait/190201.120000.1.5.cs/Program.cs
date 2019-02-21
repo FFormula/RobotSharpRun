8448e3ce52ae5e04d4adc472e38ac38d8baec241
Jevgenij Volosatov <fformula@gmail.com>
2019-02-21 10:57:59 -0800

    Demo requests

---
+++ RobotSharpRun/tasks/wait/190201.120000.1.5.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = Convert.ToInt16(Console.ReadLine());
+        int s = a + b;
+        Console.WriteLine(s);
+    }
+}
---
+++ RobotSharpRun/tasks/wait/190202.150000.1.5.java/Program.java
@@ -0,0 +1,13 @@
+﻿import java.util.Scanner;
+
+public class Program
+{
+	public static void main(String ... args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int s = a + b;
+		System.out.pritnln(s);
+	}
+}
