7b931ae9acb1302083375df89afccef23d12b6a9
Jevgenij Volosatov <fformula@gmail.com>
2019-03-03 19:33:43 +0200

    cpp and pascal

---
+++ RobotSharpRun/Robots/RobotCpp.cs
@@ -0,0 +1,29 @@
+﻿namespace RobotSharpRun.Robots
+{
+    using RobotSharpRun.Services;
+
+    class RobotCpp : ARobot
+    {
+        private readonly string GccExe;
+
+        public RobotCpp(CmdDriver cmd, string RunFolder, string GccExe, string DenyWords)
+            : base(cmd, RunFolder,
+                  "Program.cpp",
+                  "Program.exe",
+                  DenyWords)
+        {
+            this.GccExe = GccExe;
+        }
+
+        public override void Compile()
+        {
+            cmd.Run($@"""{GccExe}"" {SourceFile} -o {ExecFile} 2> {CompilerOut}");
+        }
+
+        public override void RunTest(string inFile, string outFile)
+        {
+            cmd.Run($@"{ExecFile} < {inFile} > {outFile} 2>&1");
+        }
+
+    }
+}
---
+++ RobotSharpRun/Robots/RobotPascal.cs
@@ -0,0 +1,32 @@
+﻿namespace RobotSharpRun.Robots
+{
+    using RobotSharpRun.Services;
+    using System.IO;
+
+    class RobotPascal : ARobot
+    {
+        private readonly string FpcExe;
+
+        public RobotPascal(CmdDriver cmd, string RunFolder, string FpcExe, string DenyWords)
+            : base(cmd, RunFolder,
+                  "Program.pas",
+                  "Program.exe",
+                  DenyWords)
+        {
+            this.FpcExe = FpcExe;
+        }
+
+        public override void Compile()
+        {
+            cmd.Run($@"""{FpcExe}"" {SourceFile} > {CompilerOut}");
+            if (!File.ReadAllText(RunFolder + CompilerOut).Contains("error"))
+                File.WriteAllText(RunFolder + CompilerOut, "");
+        }
+
+        public override void RunTest(string inFile, string outFile)
+        {
+            cmd.Run($@"""{ExecFile}"" < {inFile} > {outFile} 2>&1");
+        }
+
+    }
+}
